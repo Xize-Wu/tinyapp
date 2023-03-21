24a0641989224a4f900572faf4af1f8aa60aca45
Xize Wu <wuxize1995@gmail.com>
2023-03-20 20:17:27 -0400

    update

--- express_server.js
+++ express_server.js
@@ -10,7 +10,10 @@ app.get("/", (req, res) => { res.send("Hello!"); });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-  res.send(urlDatabase)
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
