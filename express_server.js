a1a4e18be11a9335318dd280b259f3dbfa1f7d7d
Xize Wu <wuxize1995@gmail.com>
2023-03-20 21:57:37 -0400

    dead link added

--- express_server.js
+++ express_server.js
@@ -4,20 +4,27 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");//declares the ejs as the templating engine
 
+// app.get("/", (req, res) => { res.send("Hello!"); });
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => { res.send("Hello!"); });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
